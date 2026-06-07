c9534766ba61edfca541a1e2b44f99e007078b60
shreypandarathilcs24 <shreypandarathil.cs24@bmsce.ac.in>
2026-06-07 23:15:49 +0530

    Update RoundRobin.c

--- OS-Lab/LP-4/RoundRobin.c
+++ OS-Lab/LP-4/RoundRobin.c
@@ -2,64 +2,76 @@
 
 int main() {
     int n, tq;
-    int at[10], bt[10], rt[10];
-    int ct[10], tat[10], wt[10];
-    int time = 0, remain, i;
-    float avg_tat = 0, avg_wt = 0;
 
-    printf("Enter the number of processes.\n");
+    printf("Enter number of processes: ");
     scanf("%d", &n);
-    remain = n;
 
-    printf("Enter Arrival Time and Burst Time.\n");
-    for (i = 0; i < n; i++) {
-        printf("Process %d AT and BT: ", i + 1);
-        scanf("%d %d", &at[i], &bt[i]);
+    int at[n], bt[n], rt[n], ct[n], tat[n], wt[n];
+
+    for(int i = 0; i < n; i++) {
+        printf("\nProcess P%d\n", i + 1);
+
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+
+        printf("Burst Time: ");
+        scanf("%d", &bt[i]);
+
         rt[i] = bt[i];
     }
 
-    printf("Enter the quantum time: ");
+    printf("Enter Time Quantum: ");
     scanf("%d", &tq);
 
-    while (remain != 0) {
-        int done_something = 0;
+    int completed = 0, time = 0;
 
-        for (i = 0; i < n; i++) {
-            if (rt[i] > 0 && at[i] <= time) {
-                done_something = 1;
+    while(completed < n) {
 
-                if (rt[i] > tq) {
+        int found = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(at[i] <= time && rt[i] > 0) {
+
+                found = 1;
+
+                if(rt[i] > tq) {
                     time += tq;
                     rt[i] -= tq;
-                } else {
+                }
+                else {
                     time += rt[i];
-                    ct[i] = time;
                     rt[i] = 0;
-                    remain--;
+
+                    ct[i] = time;
+                    tat[i] = ct[i] - at[i];
+                    wt[i] = tat[i] - bt[i];
+
+                    completed++;
                 }
             }
         }
 
-        if (done_something == 0) {
+        if(!found)
             time++;
-        }
     }
 
-    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
+    float avg_tat = 0, avg_wt = 0;
 
-    for (i = 0; i < n; i++) {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+
+    for(int i = 0; i < n; i++) {
 
         printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
-               i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+               i + 1, at[i], bt[i],
+               ct[i], tat[i], wt[i]);
 
         avg_tat += tat[i];
         avg_wt += wt[i];
     }
 
-    printf("\nAverage Turnaround Time = %.2f", avg_tat / n);
-    printf("\nAverage Waiting Time = %.2f\n", avg_wt / n);
+    printf("\nAverage TAT = %.2f", avg_tat / n);
+    printf("\nAverage WT = %.2f\n", avg_wt / n);
 
     return 0;
 }
