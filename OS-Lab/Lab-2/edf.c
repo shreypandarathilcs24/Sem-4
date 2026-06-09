ced0f040d01e2ec60bc91cdf372e9d1896f80b94
shreypandarathilcs24 <shreypandarathil.cs24@bmsce.ac.in>
2026-06-10 05:12:01 +0530

    Update edf.c

--- OS-Lab/Lab-2/edf.c
+++ OS-Lab/Lab-2/edf.c
@@ -1,40 +1,48 @@
 #include <stdio.h>
 
-struct Process {
-    int pid;
-    int ci;   // execution time
-    int di;   // deadline
-};
-
 int main() {
-    int n;
-    float u = 0;
+    int n, time, earliest;
 
-    printf("Enter number of processes: ");
+    printf("Enter number of tasks: ");
     scanf("%d", &n);
 
-    struct Process p[n];
+    int c[n], d[n], rt[n];
 
-    for (int i = 0; i < n; i++) {
-        p[i].pid = i + 1;
+    for(int i = 0; i < n; i++) {
+        printf("\nTask %d\n", i + 1);
 
-        printf("\nProcess %d\n", i + 1);
+        printf("Execution Time: ");
+        scanf("%d", &c[i]);
 
-        printf("Enter Execution Time (Ci): ");
-        scanf("%d", &p[i].ci);
+        printf("Deadline: ");
+        scanf("%d", &d[i]);
 
-        printf("Enter Deadline (Di): ");
-        scanf("%d", &p[i].di);
-
-        u += (float)p[i].ci / p[i].di;
+        rt[i] = c[i];
     }
 
-    printf("\nCPU Utilization = %.4f\n", u);
+    printf("\nSchedule:\n|");
 
-    if (u <= 1)
-        printf("Schedulable using EDF\n");
-    else
-        printf("Not Guaranteed\n");
+    while(1) {
+
+        earliest = -1;
+
+        for(int i = 0; i < n; i++) {
+            if(rt[i] > 0) {
+                if(earliest == -1 || d[i] < d[earliest])
+                    earliest = i;
+            }
+        }
+
+        if(earliest == -1)
+            break;
+
+        printf(" T%d |", earliest + 1);
+
+        rt[earliest]--;
+        time++;
+    }
+
+    printf("\n");
 
     return 0;
 }
